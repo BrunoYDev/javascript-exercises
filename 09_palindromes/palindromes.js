7bae13039ae853817c94ea295811477590055f52
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2024-08-03 19:40:15 -0300

    Add: Exercises 8 and 9 solutions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,28 @@
-const palindromes = function () {
+const palindromes = function (theWord) {
+    let palindrome = theWord.toLowerCase().replace(/\s+/g, "");
 
+    const testRegex = /[.\/#!$%\^&\*;:{}=\-_~()]/g;
+
+    if(palindrome.match(testRegex)){
+        palindrome = palindrome.replace(testRegex, "");
+    }
+
+    if(palindrome.includes(",")){
+        let moreThanOneWord = palindrome.replace(/,/g, "");
+
+        if(reverseString(moreThanOneWord) === moreThanOneWord){
+            return true;
+        }
+        return false;
+    }
+
+    if(reverseString(palindrome) === palindrome) return true
+    
+    return false;
+}; 
+
+const reverseString = (theString) => {
+    return theString.split('').reverse().join('');
 };
 
 // Do not edit below this line
